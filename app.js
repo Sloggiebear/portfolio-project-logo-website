5cda42c03c643620347b52ac16691669ca8d7942
Logan McLain <Sloggiebear@users.noreply.github.com>
2022-01-25 14:07:30 +0000

    updating styles and content

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Change nav style on scroll
+let nav = document.getElementById("nav");
 window.addEventListener('scroll', event => {
     let nav = document.querySelector('nav');
     (window.scrollY >= 50) ? nav.classList.add('nav-scrolled') : nav.classList.remove('nav-scrolled');
